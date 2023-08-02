652c24108bfbbbe292e5ca2d41690c03e6cf0a7c
MoisesFC <37132345+MoisesFC@users.noreply.github.com>
2023-08-02 19:54:54 -0400

    added cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,44 +8,44 @@ import Button from 'react-bootstrap/Button';
 
 
 const Header = () => {
-    return(
-        <Navbar data-bs-theme="dark" className='navdark'expand="md"  >
-          <Navbar.Brand style={{paddingLeft: "20px"}} href="#home">
-          <img
-              src={SummitBankLogo}
-              height="60"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle style={{margin: '10px'}} className="toggler-icon"   aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse  id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link className="navLink" href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Checking</Nav.Link>
-              <Nav.Link href="#link">Credit</Nav.Link>
-              <Nav.Link href="#link">Retirement</Nav.Link>
-            </Nav>
-            <Form className="d-flex" data-bs-theme="light">
-            <Form.Control
-              type="username"
-              placeholder="Username"
-              className="me-2"
-              aria-label="username"
-            />            
-            <Form.Control
+  return (
+    <Navbar data-bs-theme="dark" className='navdark' expand="md"  >
+      <Navbar.Brand style={{ paddingLeft: "20px" }} href="#home">
+        <img
+          src={SummitBankLogo}
+          height="60"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle style={{ margin: '10px' }} className="toggler-icon" aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link className="navLink" href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Checking</Nav.Link>
+          <Nav.Link href="#link">Credit</Nav.Link>
+          <Nav.Link href="#link">Retirement</Nav.Link>
+        </Nav>
+        <Form className="d-flex" data-bs-theme="light">
+          <Form.Control
+            type="username"
+            placeholder="Username"
+            className="me-2"
+            aria-label="username"
+          />
+          <Form.Control
             type="password"
             placeholder="Password"
             className="me-2"
             aria-label="password"
-            />
-          </Form>
-          <Button style={{margin: "10px"}} lg variant="light">Log In</Button>
-          
-          <Button style={{margin: '10px'}} lg variatn="light">Create Online Account</Button>
-          </Navbar.Collapse>
-      </Navbar>
-    )
+          />
+        </Form>
+        <Button style={{ margin: "10px" }} lg variant="light">Log In</Button>
+
+        <Button style={{ margin: '10px' }} lg variatn="light">Create Online Account</Button>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
 export default Header;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,35 +1,86 @@
-import { Carousel } from "react-bootstrap"
+import {
+    Carousel,
+    Row,
+    Col,
+    Card,
+    Container
+} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import coinImage from '../img/moneyCoins.jpg';
 import houseImage from '../img/houseWide.jpg';
 import creditCardImage from '../img/creditCardWide.jpg'
+import carImg from '../img/car.jpg';
+import piggieImg from '../img/piggie.jpg';
+import checkingImg from '../img/checking.jpg';
 
 const HomePage = () => {
     return (
-        <div className="carousel-container">
-        <Carousel>
-          <Carousel.Item>
-            <img src={coinImage} className="carousel-image" alt="Coin" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src={houseImage} className="carousel-image" alt="House" />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src={creditCardImage} className="carousel-image" alt="Credit Card" />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
+        <Container fluid className="p-0">
+        <div >
+            <Carousel className="mx-0">
+                <Carousel.Item>
+                    <img src={coinImage} className="carousel-image" alt="Coin" />
+                    <Carousel.Caption className="carousel-caption-content">
+                        <h3>High Yield CDs Accounts</h3>
+                        <p className="hide-on-small-screen">Grow your savings with our high-yield CD rates, featuring guaranteed returns and flexible terms to fit your financial goals.</p>
+                    </Carousel.Caption >
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={houseImage} className="carousel-image" alt="House" />
+                    <Carousel.Caption className="carousel-caption-content">
+                        <h3>Home Mortgage</h3>
+                        <p className="hide-on-small-screen">Make your dream home a reality with our home mortgage offer, featuring competitive rates, flexible payment options, and expert guidance from start to finish.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={creditCardImage} className="carousel-image" alt="Credit Card" />
+                    <Carousel.Caption className="carousel-caption-content">
+                        <h3>Summit Platinum Card</h3>
+                        <p className="hide-on-small-screen">Get the financial freedom you deserve with our credit card, featuring low introductory interest rates, rewards for every purchase, and worldwide fraud protection.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <br />
+            <Row className="cards-row px-0 px-md-4">
+                <Col xs={12} md={4}>
+                    <Card className="mb-4">
+                        <Card.Img variant="top" src={checkingImg} />
+                        <Card.Body>
+                            <Card.Title>Checking</Card.Title>
+                            <Card.Text>
+                            Simplify your finances and take control of your money with our checking account offer, featuring no monthly fees, free online banking and bill pay, and access to over thousands of ATMs nationwide. With our account, you can manage your money with ease and enjoy the convenience of banking on your terms.
+                            </Card.Text>
+                            <Button variant="primary">Open Account</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xs={12} md={4}>
+                    <Card className="mb-4">
+                        <Card.Img variant="top" src={piggieImg} />
+                        <Card.Body>
+                            <Card.Title>Savings</Card.Title>
+                            <Card.Text>
+                            Start saving for your future today with our savings account offer, featuring competitive interest rates and no monthly fees, plus easy access to your funds whenever you need them. With our account, you can achieve your financial goals and enjoy peace of mind knowing your money is safe and secure.
+                            </Card.Text>
+                            <Button variant="primary">Start Saving</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xs={12} md={4}>
+                    <Card className="mb-4">
+                        <Card.Img variant="top" src={carImg} />
+                        <Card.Body>
+                            <Card.Title>Car Loans</Card.Title>
+                            <Card.Text>
+                            Start saving for your future today with our savings account offer, featuring competitive interest rates and no monthly fees, plus easy access to your funds whenever you need them. With our account, you can achieve your financial goals and enjoy peace of mind knowing your money is safe and secure.
+                            </Card.Text>
+                            <Button variant="primary">Check Rates</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+        </Container>
     )
 };
 
